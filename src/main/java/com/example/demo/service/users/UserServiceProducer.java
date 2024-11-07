814afcd8c2709d27e669d09e307cb4f63016e0d3
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-11-07 13:36:19 +0700

    fix userRequest getinfo

--- src/main/java/com/example/demo/service/users/UserServiceProducer.java
+++ src/main/java/com/example/demo/service/users/UserServiceProducer.java
@@ -27,22 +27,25 @@ public class UserServiceProducer {
 
 
     public String getInfo(String authorizationHeader) throws JsonProcessingException {
-
         UserRequest userRequest = new UserRequest(authorizationHeader);
         CompletableFuture<String> futureResponse = new CompletableFuture<>();
         pendingRequests.put(userRequest.getId(), futureResponse);
 
         try {
-            kafkaProducer.sendMessage("user_info", userRequest.toString());
-            String message = futureResponse.get(10, TimeUnit.SECONDS);
-            System.err.println("user_info Response: " + message);
-            return message;
+            System.err.println("user_info Request: " + userRequest);
+            String message = new ObjectMapper().writeValueAsString(userRequest);
+            kafkaProducer.sendMessage("user_info", message);
+
+            String responseMessage = futureResponse.get(10, TimeUnit.SECONDS);
+            System.err.println("user_info Response: " + responseMessage);
+            return responseMessage;
         } catch (TimeoutException | InterruptedException | ExecutionException e) {
-            throw new RuntimeException("Timeout waiting for response from auth user");
+            throw new RuntimeException("Timeout waiting for response from auth user", e);
         } finally {
             pendingRequests.remove(userRequest.getId());
         }
     }
+
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -52,17 +55,6 @@ public class UserServiceProducer {
 
         public UserRequest(String authorizationHeader){
             this.authorizationHeader = authorizationHeader;
-        }
-
-        @Override
-        public String toString() {
-            String json;
-            try {
-                json = (new ObjectMapper()).writeValueAsString(this);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-            return json;
         }
     }
 }
