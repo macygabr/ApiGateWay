8c6effa2e27b7d67759e918f3e5a2bcdb672442a
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-11-07 14:52:33 +0700

    fix auth

--- src/main/java/com/example/demo/service/authentication/AuthenticationServiceConsumer.java
+++ src/main/java/com/example/demo/service/authentication/AuthenticationServiceConsumer.java
@@ -2,6 +2,7 @@ package com.example.demo.service.authentication;
 
 import com.example.demo.models.AuthenticationServerResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +16,13 @@ public class AuthenticationServiceConsumer {
         this.authService = authService;
     }
     @KafkaListener(topics = "auth_response", groupId = "auth_service")
-    public void consumeMessage(String message) {
+    public void consumeMessage(ConsumerRecord<String, String> message) {
 
-        AuthenticationServerResponse response = new AuthenticationServerResponse(message);
-        CompletableFuture<String> futureResponse = authService.getPendingRequests().get(response.getId());
-
-        if (futureResponse != null && response.getId() != null) {
-            futureResponse.complete(message);
+        CompletableFuture<String> futureResponse = authService.getPendingRequests().get(message.key());
+        if (futureResponse != null) {
+            futureResponse.complete(message.value());
         } else {
-            System.err.println("No matching request found for response: " + response);
+            System.err.println("No matching request found: " + message.key());
         }
     }
 }
