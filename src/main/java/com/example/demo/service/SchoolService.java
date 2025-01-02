1e4a79c5ab7dd2f8d3a0ac9e9b3e06c7029492db
Macy Gabrielle <macygabr@si-c8.nsk.21-school.ru>
2025-01-02 18:28:58 +0700

    fix peer Controller

--- src/main/java/com/example/demo/service/SchoolService.java
+++ src/main/java/com/example/demo/service/SchoolService.java
@@ -1,19 +1,38 @@
 package com.example.demo.service;
 
 import com.example.demo.models.s21.filter.FilterS21;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class SchoolService {
     private final KafkaService kafkaService;
     private final Logger logger = LoggerFactory.getLogger(SchoolService.class);
+    private final ObjectMapper objectMapper;
 
     public String peers(int page, int size, String campusId, FilterS21 filter) {
-        logger.debug("Получение списка пиров: page={}, size={}, campusId={}, filter={}", page, size, campusId, filter);
-        return kafkaService.sendRequest("peers");
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("page", page);
+        jsonMap.put("size", size);
+        jsonMap.put("campusId", campusId);
+        jsonMap.put("filter", filter.toString());
+
+        try {
+            String json = objectMapper.writeValueAsString(jsonMap);
+            logger.debug("Сформированный JSON: {}", json);
+
+            return kafkaService.sendRequest("peers", json);
+        } catch (JsonProcessingException e) {
+            logger.error("Ошибка сериализации объекта в JSON", e);
+            throw new RuntimeException("Не удалось создать JSON", e);
+        }
     }
 }
